dd3c67f612dda1916fc5d836678424893cf55ca3
violet <1936800723@qq.com>
2026-04-20 21:07:42 +0800

    基于LangChain4J的项目

---
+++ src/test/java/com/vergissmeinnicht/java/ai/langchain4j/LLMTest.java
@@ -0,0 +1,19 @@
+package com.vergissmeinnicht.java.ai.langchain4j;
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LLMTest {
+    @Test
+    public void testGPTDemo(){
+        OpenAiChatModel model = OpenAiChatModel.builder()
+                .baseUrl("http://langchain4j.dev/demo/openai/v1")
+                .apiKey("demo")
+                .modelName("gpt-4o-mini")
+                .build();
+        String answer = model.chat("你是谁？");
+        System.out.println(answer);
+    }
+}
