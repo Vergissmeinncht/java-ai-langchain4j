20bc25205e56ad1d9d04b0b7b68e935b9da19376
violet <1936800723@qq.com>
2026-04-22 20:55:45 +0800

    利用MongoDB实现记忆持久化

--- src/main/java/com/vergissmeinnicht/java/ai/langchain4j/assistant/SeparateChatAssistant.java
+++ src/main/java/com/vergissmeinnicht/java/ai/langchain4j/assistant/SeparateChatAssistant.java
@@ -13,7 +13,7 @@ import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
  */
 @AiService(
         wiringMode = EXPLICIT,
-        chatModel = "ollamaChatModel",
+        chatModel = "qwenChatModel",
         chatMemoryProvider = "chatMemoryProvider"
 )
 public interface SeparateChatAssistant {
