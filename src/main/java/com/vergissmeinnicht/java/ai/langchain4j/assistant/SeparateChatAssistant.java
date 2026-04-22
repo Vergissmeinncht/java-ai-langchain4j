ae2a0a0aac0d32f3cf01086507e7e2342d0172b4
violet <1936800723@qq.com>
2026-04-22 09:21:44 +0800

    隔离聊天

---
+++ src/main/java/com/vergissmeinnicht/java/ai/langchain4j/assistant/SeparateChatAssistant.java
@@ -0,0 +1,21 @@
+package com.vergissmeinnicht.java.ai.langchain4j.assistant;
+
+import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.spring.AiService;
+import dev.langchain4j.service.spring.AiServiceWiringMode;
+
+import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
+
+
+/**
+ * 根据记忆id隔离聊天记忆的智能体
+ */
+@AiService(
+        wiringMode = EXPLICIT,
+        chatModel = "ollamaChatModel",
+        chatMemoryProvider = "chatMemoryProvider"
+)
+public interface SeparateChatAssistant {
+    String chat(@MemoryId int memoryId,@UserMessage String userMessage);
+}
