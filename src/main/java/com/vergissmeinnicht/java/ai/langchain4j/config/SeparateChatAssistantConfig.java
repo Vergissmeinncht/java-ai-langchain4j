c5f44a774d17f65b26295c94301d8cd981914b46
violet <1936800723@qq.com>
2026-04-22 20:47:31 +0800

    利用MongoDB实现记忆持久化

--- src/main/java/com/vergissmeinnicht/java/ai/langchain4j/config/SeparateChatAssistantConfig.java
+++ src/main/java/com/vergissmeinnicht/java/ai/langchain4j/config/SeparateChatAssistantConfig.java
@@ -1,15 +1,26 @@
 package com.vergissmeinnicht.java.ai.langchain4j.config;
 
+import com.vergissmeinnicht.java.ai.langchain4j.store.MongoChatMemoryStore;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SeparateChatAssistantConfig {
 
+    @Autowired
+    private MongoChatMemoryStore mongoChatMemoryStore;
+
     @Bean
     public ChatMemoryProvider chatMemoryProvider(){
-        return memoryId -> MessageWindowChatMemory.builder().id(memoryId).maxMessages(10).build();
+        return memoryId -> MessageWindowChatMemory
+                .builder()
+                .id(memoryId)
+                .maxMessages(10)
+                .chatMemoryStore(mongoChatMemoryStore)
+                .build();
     }
 }
