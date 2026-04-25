38908289a8b4709d500b6d7b96f312a1a69d4fb4
violet <1936800723@qq.com>
2026-04-25 13:59:40 +0800

    预约业务开发

---
+++ src/main/java/com/vergissmeinnicht/java/ai/langchain4j/service/AppointmentService.java
@@ -0,0 +1,9 @@
+package com.vergissmeinnicht.java.ai.langchain4j.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.vergissmeinnicht.java.ai.langchain4j.entity.Appointment;
+
+
+public interface AppointmentService extends IService<Appointment> {
+    Appointment getOne(Appointment appointment);
+}
